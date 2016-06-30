bfa782597ee50adff2872b28ec165ce2b2058c8d
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-06-30 13:54:04 -0400

    did stuff

---
+++ Library/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book : Media
+    {
+        public override int RentalLength
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void PrintMediaDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Library/Media.cs
+++ Library/Media.cs
@@ -10,10 +10,10 @@ namespace Library
     {
         public virtual string Title { get;  set; }
         public virtual string Length { get;  set; }
-        public int RentalLength { get; set; }
         protected DateTime RentedDate { get;  set; }
-    
-        
+        public abstract int RentalLength { get; set; }
+
+
         public Media()
         {
             RentedDate = DateTime.Today;
@@ -24,6 +24,8 @@ namespace Library
         public abstract DateTime ReturnDueDate();
 
         public virtual void  SetRentedDate()
+
+
         {
             RentedDate = DateTime.Today;
         }
