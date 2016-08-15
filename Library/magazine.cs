f3c2fd5d278be2d380e90e2fa643732831744484
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-08-14 21:09:16 -0400

    final file

---
+++ Library/magazine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class magzine : Media
+    {
+        public override int RentalLength
+        {
+            get
+            {
+                return 7;
+            }
+            //Ze set mezod, it does nozing! boot
+            set
+            {
+
+            }
+        }
+
+        public override void PrintMediaDetails()
+        {
+            Console.WriteLine("DVD, {0}, ({1})", Title, Length);
+            Console.WriteLine("Rented on: {0}", RentedDate);
+            Console.WriteLine("Due on: {0}", ReturnDueDate());
+        }
+
+
+
+        public override DateTime ReturnDueDate()
+        {
+            return RentedDate.AddDays(RentalLength);
+        }
+    }
+}
